3b6ff730a929dc12809adae9f0bb121e1f244679
Pedro Tunin <pedrotunin@gmail.com>
2024-05-21 11:52:47 -0300

    feat: update remaps

--- lua/tunin/remap.lua
+++ lua/tunin/remap.lua
@@ -1,18 +1,28 @@
+-- open file explorer
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- move lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- keep cursor centralized
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- yank to clipboard
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>y", "\"+Y")
 
-vim.keymap.set("n", "<leader>f", function()
-    vim.lsp.buf.format()
-end)
+-- format file
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- replace current word for another
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+
